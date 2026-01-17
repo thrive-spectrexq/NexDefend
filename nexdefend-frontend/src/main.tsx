7308f1633d559faf47f7166aa555eb0cc13d2ee8
Bright Spectre <143046193+thrive-spectrexq@users.noreply.github.com>
2026-01-17 17:55:52 +0000

    Update main.tsx

--- nexdefend-frontend/src/main.tsx
+++ nexdefend-frontend/src/main.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import { ThemeProvider, CssBaseline } from '@mui/material';
-// import { store } from '@/store';
-// import theme from '@/theme';
-// import AppRoutes from '@/routes';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/*
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppRoutes />
-      </ThemeProvider>
+      <App />
     </Provider>
-    */}
-    <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
